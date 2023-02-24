6ef6527b6644789905f4547a1dbc5e6dd3e3d459
Edu <eduardo.ex7@gmail.com>
2023-02-24 17:59:53 -0300

    Alt. 1

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { runOnJS } from 'react-native-reanimated';
+
+import { StyleSheet } from 'react-native';
+import {
+  Camera,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+
+//import { Camera } from 'react-native-vision-camera';
+import { scanFaces, Face } from 'vision-camera-face-detector';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [faces, setFaces] = React.useState<Face[]>();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const devices = useCameraDevices();
+  const device = devices.front;
+
+  React.useEffect(() => {
+    console.log(faces);
+  }, [faces]);
+
+  React.useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet';
+    const scannedFaces = scanFaces(frame);
+    runOnJS(setFaces)(scannedFaces);
+  }, []);
+
+  return device != null && hasPermission ? (
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+      frameProcessorFps={5}
+    />
+  ) : null;
+}
